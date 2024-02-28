9449ebbb2aef8bc0ba2702c50de8e29f57f4fa18
sudo-m3z1m <nizilplay2004@gmail.com>
2024-02-29 00:32:44 +0300

    Test commit, renamed crowd speed_decrease_multiplier variable.

--- Prefabs/Univercity/crowd.gd
+++ Prefabs/Univercity/crowd.gd
@@ -3,7 +3,7 @@ extends Node3D
 @onready var area: Area3D = $CrowdArea
 
 @export_category("Crowd properties")
-@export_range(0, 1) var speed_debuff_multiplier: float
+@export_range(0, 1) var speed_decrease_multiplier: float
 
 func _ready() -> void:
 	area.body_entered.connect(on_player_entered)
@@ -12,9 +12,9 @@ func _ready() -> void:
 func on_player_entered(player: Node3D) -> void:
 	if !(player is Player):
 		return
-	player.speed *= speed_debuff_multiplier
+	player.speed *= speed_decrease_multiplier
 
 func on_player_exited(player: Node3D) -> void:
 	if !(player is Player):
 		return
-	player.speed /= speed_debuff_multiplier
+	player.speed /= speed_decrease_multiplier
